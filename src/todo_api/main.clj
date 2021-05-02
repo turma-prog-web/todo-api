b9917d93f47aa3b41a13e9ac144c23341a309bb7
Carlos Alberto <carloshtcursos@gmail.com>
2021-05-02 03:24:37 -0400

    create default routes

--- src/todo_api/main.clj
+++ src/todo_api/main.clj
@@ -1,7 +1,11 @@
 (ns todo-api.main
   (:require [compojure.core :refer [defroutes routes]]
+            [compojure.route :as route]
             [clojure.walk :refer [prewalk prewalk-demo]])
   (:gen-class :main true))
 
-
-(def)
+(defroutes default
+           (route/resources "/")
+           (route/not-found "Not Found"))
+(def app
+  (-> (routes default)))
